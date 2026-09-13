829827d50b9959b3a4fc934514e1b7072c8ae1c9
mercysiu <crush121006@gmail.com>
2026-09-13 10:56:42 +0700

    Time: 0 ms (100.00%) | Memory: 12.7 MB (5.13%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,10 +1,25 @@
 import math
 class Solution(object):
     def uniquePaths(self, m, n):
-        #case1
-        SoT = math.factorial(m + n -2)
-        k = math.factorial(m -1)
-        neg = math.factorial(n-1)
-        return (SoT)/(k*neg)
+        #case1: C(n, k)
+        # SoT = math.factorial(m + n -2)
+        # k = math.factorial(m -1)
+        # neg = math.factorial(n-1)
+        # return (SoT)/(k*neg)
+        #case2: dynamic programming
+        pscase_rn = {}
+        def recur(up, left):
+            if up < 0 or left < 0:
+                return 0
+            if up == 0 or left == 0:
+                return 1
+            if (up, left) in pscase_rn:
+                return pscase_rn[(up,left)]
+            pscase_rn[(up, left)] = recur(up-1, left) + recur(up, left-1)
+            return pscase_rn[(up,left)]
+        return recur(m -1, n -1)
+
+            
+
 
         
